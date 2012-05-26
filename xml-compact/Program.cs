8375cdd34091856c599304872c71eef1dcddadc4
Jeffrey Bush <jeff@coderforlife.com>
2012-05-26 13:22:11 -0700

    Adding xml-compact utility.

---
+++ xml-compact/Program.cs
@@ -0,0 +1,58 @@
+﻿// xml-compact: XML compactor (white-space remover)
+// Copyright (C) 2011  Jeffrey Bush <jeff@coderforlife.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/gpl.html>.
+
+
+using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace xml_compact
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("xml-compact Copyright (C) 2011  Jeffrey Bush <jeff@coderforlife.com>");
+            Console.WriteLine("This program comes with ABSOLUTELY NO WARRANTY;");
+            Console.WriteLine("This is free software, and you are welcome to redistribute it");
+            Console.WriteLine("under certain conditions;");
+            Console.WriteLine("See http://www.gnu.org/licenses/gpl.html for more details.");
+            Console.WriteLine();
+
+            if (args.Length != 2) {
+                Console.WriteLine("Must provide input and output XML files on the command line");
+                return;
+            }
+
+            try {
+                string txt = File.ReadAllText(args[0], Encoding.UTF8);
+
+                txt = Regex.Replace(txt, @"<!\s*--([^-]|-[^-])*--\s*>", "");     // comments
+                txt = Regex.Replace(txt, @"(['""\w])\s+(/?>)",          "$1$2"); // extra spaces at the ends of tags
+                txt = Regex.Replace(txt, @">\s+",                       ">");    // extra spaces after >
+                txt = Regex.Replace(txt, @"\s+<",                       "<");    // extra spaces before <
+                txt = Regex.Replace(txt, @"\s+",                        " ");    // all multi-spaces
+
+                File.WriteAllText(args[1], txt, Encoding.UTF8);
+            } catch (Exception e) {
+                Console.Error.WriteLine("Error: "+e.Message);
+            }
+
+            return;
+        }
+    }
+}
---
+++ xml-compact/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("xml-compact")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Coder For Life")]
+[assembly: AssemblyProduct("xml-compact")]
+[assembly: AssemblyCopyright("Copyright © Coder For Life 2011 GPLv3")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("af97838a-bf8c-4e9c-a8f1-977b456892e4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
