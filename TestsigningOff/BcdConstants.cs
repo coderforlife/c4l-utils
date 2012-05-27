43da0bfb22eb013a692c3b4a5c9c64c229ea02bf
Jeffrey Bush <jeff@coderforlife.com>
2012-05-26 20:01:43 -0700

    Adding TestsigningOff utility.

---
+++ TestsigningOff/BcdConstants.cs
@@ -0,0 +1,148 @@
+﻿// All enums are copied from http://msdn.microsoft.com/en-us/library/cc441427%28VS.85%29.aspx
+// The GUIDs were found at http://technet.microsoft.com/en-us/magazine/2008.07.heyscriptingguy.aspx
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BcdConstants
+{
+    class Guids
+    {
+        public const string SystemStore = "{9dea862c-5cdd-4e70-acc1-f32b344d4795}";
+        public const string Current = "{fa926493-6f1c-4193-a414-58f0b2456d1e}";
+    }
+
+    enum BcdBootMgrElementTypes : uint
+    {
+        ObjectList_DisplayOrder        = 0x24000001,
+        ObjectList_BootSequence        = 0x24000002,
+        Object_DefaultObject           = 0x23000003,
+        Integer_Timeout                = 0x25000004,
+        Boolean_AttemptResume          = 0x26000005,
+        Object_ResumeObject            = 0x23000006,
+        ObjectList_ToolsDisplayOrder   = 0x24000010,
+        Device_BcdDevice               = 0x21000022,
+        String_BcdFilePath             = 0x22000023,
+    }
+
+    enum BcdDeviceObjectElementTypes : uint
+    {
+        Integer_RamdiskImageOffset   = 0x35000001,
+        Integer_TftpClientPort       = 0x35000002,
+        Integer_SdiDevice            = 0x31000003,
+        Integer_SdiPath              = 0x32000004,
+        Integer_RamdiskImageLength   = 0x35000005,
+    }
+
+    enum BcdLibraryElementTypes : uint
+    {
+        Device_ApplicationDevice                   = 0x11000001,
+        String_ApplicationPath                     = 0x12000002,
+        String_Description                         = 0x12000004,
+        String_PreferredLocale                     = 0x12000005,
+        ObjectList_InheritedObjects                = 0x14000006,
+        Integer_TruncatePhysicalMemory             = 0x15000007,
+        ObjectList_RecoverySequence                = 0x14000008,
+        Boolean_AutoRecoveryEnabled                = 0x16000009,
+        IntegerList_BadMemoryList                  = 0x1700000a,
+        Boolean_AllowBadMemoryAccess               = 0x1600000b,
+        Integer_FirstMegabytePolicy                = 0x1500000c,
+        Boolean_DebuggerEnabled                    = 0x16000010,
+        Integer_DebuggerType                       = 0x15000011,
+        Integer_SerialDebuggerPortAddress          = 0x15000012,
+        Integer_SerialDebuggerPort                 = 0x15000013,
+        Integer_SerialDebuggerBaudRate             = 0x15000014,
+        Integer_1394DebuggerChannel                = 0x15000015,
+        String_UsbDebuggerTargetName               = 0x12000016,
+        Boolean_DebuggerIgnoreUsermodeExceptions   = 0x16000017,
+        Integer_DebuggerStartPolicy                = 0x15000018,
+        Boolean_EmsEnabled                         = 0x16000020,
+        Integer_EmsPort                            = 0x15000022,
+        Integer_EmsBaudRate                        = 0x15000023,
+        String_LoadOptionsString                   = 0x12000030,
+        Boolean_DisplayAdvancedOptions             = 0x16000040,
+        Boolean_DisplayOptionsEdit                 = 0x16000041,
+        Boolean_GraphicsModeDisabled               = 0x16000046,
+        Integer_ConfigAccessPolicy                 = 0x15000047,
+        Boolean_DisableIntegrityChecks			 = 0x16000048,
+        Boolean_AllowPrereleaseSignatures          = 0x16000049,
+    }
+
+    enum BcdMemDiagElementTypes : uint
+    {
+        Integer_PassCount      = 0x25000001,
+        Integer_FailureCount   = 0x25000003,
+    }
+
+    enum BcdOSLoaderElementTypes : uint
+    {
+        Device_OSDevice                       = 0x21000001,
+        String_SystemRoot                     = 0x22000002,
+        Object_AssociatedResumeObject         = 0x23000003,
+        Boolean_DetectKernelAndHal            = 0x26000010,
+        String_KernelPath                     = 0x22000011,
+        String_HalPath                        = 0x22000012,
+        String_DbgTransportPath               = 0x22000013,
+        Integer_NxPolicy                      = 0x25000020,
+        Integer_PAEPolicy                     = 0x25000021,
+        Boolean_WinPEMode                     = 0x26000022,
+        Boolean_DisableCrashAutoReboot        = 0x26000024,
+        Boolean_UseLastGoodSettings           = 0x26000025,
+        Boolean_AllowPrereleaseSignatures     = 0x26000027,
+        Boolean_NoLowMemory                   = 0x26000030,
+        Integer_RemoveMemory                  = 0x25000031,
+        Integer_IncreaseUserVa                = 0x25000032,
+        Boolean_UseVgaDriver                  = 0x26000040,
+        Boolean_DisableBootDisplay            = 0x26000041,
+        Boolean_DisableVesaBios               = 0x26000042,
+        Integer_ClusterModeAddressing         = 0x25000050,
+        Boolean_UsePhysicalDestination        = 0x26000051,
+        Integer_RestrictApicCluster           = 0x25000052,
+        Boolean_UseBootProcessorOnly          = 0x26000060,
+        Integer_NumberOfProcessors            = 0x25000061,
+        Boolean_ForceMaximumProcessors        = 0x26000062,
+        Boolean_ProcessorConfigurationFlags   = 0x25000063,
+        Integer_UseFirmwarePciSettings        = 0x26000070,
+        Integer_MsiPolicy                     = 0x26000071,
+        Integer_SafeBoot                      = 0x25000080,
+        Boolean_SafeBootAlternateShell        = 0x26000081,
+        Boolean_BootLogInitialization         = 0x26000090,
+        Boolean_VerboseObjectLoadMode         = 0x26000091,
+        Boolean_KernelDebuggerEnabled         = 0x260000a0,
+        Boolean_DebuggerHalBreakpoint         = 0x260000a1,
+        Boolean_EmsEnabled                    = 0x260000b0,
+        Integer_DriverLoadFailurePolicy       = 0x250000c1,
+        Integer_BootStatusPolicy              = 0x250000E0,
+    }
+
+    enum BcdLibrary_DebuggerType
+    {
+        Serial   = 0,
+        IEEE1394 = 1,
+        Usb      = 2,
+    }
+
+    enum BcdLibrary_SafeBoot
+    {
+        Minimal    = 0,
+        Network    = 1,
+        DsRepair   = 2,
+    }
+
+    enum BcdOSLoader_NxPolicy
+    {
+        OptIn       = 0,
+        OptOut      = 1,
+        AlwaysOff   = 2,
+        AlwaysOn    = 3,
+    }
+
+    enum BcdOSLoader_PAEPolicy
+    {
+        Default        = 0,
+        ForceEnable    = 1,
+        ForceDisable   = 2,
+    }
+
+}
---
+++ TestsigningOff/Program.cs
@@ -0,0 +1,166 @@
+﻿// TestsigningOff: Turn off testsigning or nointegritychecks
+// Copyright (C) 2010  Jeffrey Bush <jeff@coderforlife.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/gpl.html>.
+
+#define DO_TESTSIGNING // when commented out does NOINTEGRITYCHECKS
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management;
+
+using WMI;
+using BcdConstants;
+
+namespace TestsigningOff
+{
+    class Program
+    {
+        
+#if DO_TESTSIGNING
+        const BcdLibraryElementTypes ID = BcdLibraryElementTypes.Boolean_AllowPrereleaseSignatures;
+        const string NAME = "TESTSIGNING";
+#else
+        const BcdLibraryElementTypes ID = BcdLibraryElementTypes.Boolean_DisableIntegrityChecks;
+        const string NAME = "NOINTEGRITYCHECKS";
+#endif
+
+        static int Main(string[] args)
+        {
+            Console.WriteLine("TestsigningOff Copyright (C) 2010-2012  Jeffrey Bush <jeff@coderforlife.com>");
+            Console.WriteLine("This program comes with ABSOLUTELY NO WARRANTY;");
+            Console.WriteLine("This is free software, and you are welcome to redistribute it");
+            Console.WriteLine("under certain conditions;");
+            Console.WriteLine("See http://www.gnu.org/licenses/gpl.html for more details.");
+            Console.WriteLine();
+
+            ConnectionOptions opts = new ConnectionOptions();
+            opts.Impersonation = ImpersonationLevel.Impersonate;
+            opts.EnablePrivileges = true;
+
+            ManagementScope scope = new ManagementScope("root\\WMI", opts);
+
+            // Open the system store
+            BcdObject store = new BcdObject(scope, Guids.SystemStore, "");
+
+            // Get the Bootmgr information
+            string bm_name = GetString(store, BcdLibraryElementTypes.String_Description);
+            bool bm_ts = Has(store, ID);
+
+            // Get the default GUID
+            string def = null;
+            try
+            {
+                def = GetString(store, BcdBootMgrElementTypes.Object_DefaultObject);
+            }
+            catch (Exception) {}
+
+            // Get the current GUID
+            BcdObject current = new BcdObject(scope, Guids.Current, "");
+            string cur = current.Id;
+
+            // Loop over all OS GUIDs
+            string[] guids = GetIds(store, BcdBootMgrElementTypes.ObjectList_DisplayOrder);
+            BcdObject[] objs = new BcdObject[guids.Length];
+            string[] names = new string[guids.Length];
+            bool[] testsignings = new bool[guids.Length];
+            bool any_ts = bm_ts;
+            int def_i = -1, cur_i = -1;
+            for (int i = 0; i < guids.Length; i++)
+            {
+                objs[i] = new BcdObject(scope, guids[i], "");
+                names[i] = GetString(objs[i], BcdLibraryElementTypes.String_Description);
+                testsignings[i] = Has(objs[i], ID);
+                any_ts |= testsignings[i];
+                if (guids[i].Equals(def)) def_i = i;
+                if (guids[i].Equals(cur)) cur_i = i;
+            }
+
+            // Final message
+            if (any_ts)
+            {
+                string msg = "You have " + NAME + " entries for the following:\n";
+                if (bm_ts)
+                    msg += "  {bootmgr} "+bm_name+"\n";
+                if (cur_i >= 0 && testsignings[cur_i])
+                    msg += "  {current} "+names[cur_i]+"\n";
+                if (def_i >= 0 && cur_i != def_i && testsignings[def_i])
+                    msg += "  {default} "+names[def_i]+"\n";
+                for (int i = 0; i < guids.Length; i++)
+                {
+                    if (i != cur_i && i != def_i && testsignings[i])
+                        msg += "  "+guids[i]+" "+names[i]+"\n";
+                }
+                msg += "If these were added with a previous version of this program you should remove them. Would you like to remove " + NAME + "? [Y/n] ";
+                Console.WriteLine(msg);
+
+                // y/n input
+                string line;
+                while ((line = Console.ReadLine()) != null)
+                {
+                    line = line.Trim();
+                    if (line.Length == 0 || line[0] == 'Y' || line[0] == 'y')
+                        break;
+                    else if (line[0] == 'n' || line[0] == 'N')
+                    {
+                        Console.WriteLine("Nothing was done.");
+                        return 0;
+                    }
+                }
+
+                // Set the data
+                if (bm_ts)	Remove(store, ID);
+                for (int i = 0; i < guids.Length; i++)
+                    if (testsignings[i])
+                        Remove(objs[i], ID);
+                Console.WriteLine("Removed " + NAME + ".");
+            }
+            else
+            {
+                Console.WriteLine("Nothing has " + NAME + ".");
+            }
+
+            return 0;
+        }
+
+        static string[] GetIds<E>(BcdObject o, E t) where E : IConvertible { return GetIds(o, Convert.ToUInt32(t)); }
+        static string[] GetIds(BcdObject o, uint t)
+        {
+            ManagementBaseObject mbo;
+            return o.GetElement(t, out mbo) ? (string[])mbo.GetPropertyValue("Ids") : null;
+        }
+
+        static string GetString<E>(BcdObject o, E t) where E : IConvertible { return GetString(o, Convert.ToUInt32(t)); }
+        static string GetString(BcdObject o, uint t)
+        {
+            ManagementBaseObject mbo;
+            return o.GetElement(t, out mbo) ? (string)mbo.GetPropertyValue("String") : null;
+        }
+
+        static bool Has<E>(BcdObject o, E t) where E : IConvertible { return Has(o, Convert.ToUInt32(t)); }
+        static bool Has(BcdObject o, uint t)
+        {
+            ManagementBaseObject mbo;
+            return o.GetElement(t, out mbo);
+        }
+
+        static void Remove<E>(BcdObject o, E t) where E : IConvertible { Remove(o, Convert.ToUInt32(t)); }
+        static void Remove(BcdObject o, uint t)
+        {
+            if (!o.DeleteElement(t))
+                throw new InvalidOperationException();
+        }
+    }
+}
